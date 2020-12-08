d1132543a5e5475a4312373e87ed80cc053b13c4
Izonix <74156028+Izonix@users.noreply.github.com>
2020-12-08 18:38:02 +0100

    Update help.js

--- help.js
+++ help.js
@@ -1,14 +1,16 @@
-	} if (message.content == "b!help") {
+ else if (message.content == "b!help") {
 		const exampleEmbed = new Discord.MessageEmbed()
 			.setColor('#1daadf')
 			.setAuthor("Need help ?", "https://imgur.com/K4OQ2Eb.png")
+			.setDescription("Merry Christmas ! 🎅")
 			.setThumbnail('https://imgur.com/K4OQ2Eb.png')
 			.addFields(
 				{ name: 'Commands:', value: '`b!help commands`'},
-				{ name: 'Moderations:', value: '`b!help moderations`'},
-				{ name: 'Links:', value: '\n\n[Invite me](https://discordapp.com/api/oauth2/authorize?client_id=656884187151728671&permissions=1916267615&scope=bot)\n[Discord server](https://discord.gg/PM8XQrE)'},
+				{ name: 'Moderations:', value: '`b!help moderations`\n\n[Invite me](https://discordapp.com/api/oauth2/authorize?client_id=656884187151728671&permissions=1916267615&scope=bot)\n[Discord server](https://discord.gg/PM8XQrE)\n[GitHub](https://github.com/Izonix/Boty)'},
 			)
 
 		message.channel.send(exampleEmbed);
+
+		//help - Commandes
 
 	}
